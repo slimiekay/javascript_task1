ead1689362c045a2d743e7f86c655fa37774e310
Ayatullahi Abdullahi <ayatkem@gmail.com>
2020-03-23 19:05:47 +0100

    complete javascript task 1

---
+++ task_1.js
@@ -0,0 +1,11 @@
+const name = "Ayatullahi";
+const courses = ["HTML", "CSS", "JAVASCRIPT"];
+
+console.log(name);
+console.log(courses);
+
+for (let i = 1; i <= 200; i++) {
+    if(i % 2 != 0) {
+        console.log(i);
+    }
+}
